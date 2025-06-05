02ef3662bc9ebd4f8c2d0bf4bc739ab578e0b63d
Palaolitico <p@seismotechnologies.com>
2025-06-05 13:10:51 +0200

    Implement decoder on IO::Buffer

---
+++ lib/seismo/maxmind/db.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Seismo
+  module MaxMind
+    # FIXME: Document
+    # Comment
+    module DB
+    end
+  end
+end
+
+require 'seismo/maxmind/db/decoder'
---
+++ lib/seismo/maxmind/db/decoder.rb
@@ -0,0 +1,211 @@
+# frozen_string_literal: true
+
+module Seismo::MaxMind::DB
+  # +Decoder+ ...
+  class BufferDecoder
+    def initialize(buf, pointer_base, offset)
+      @buf = buf
+      @pointer_base = pointer_base
+      @offset = offset
+      @follow_pointers = true
+    end
+
+    attr_accessor :pointer_base, :offset, :follow_pointers
+
+    def decode
+      i = @offset
+      control = @buf.get_value(:U8, i)
+      i += 1
+
+      type = control >> 5
+      if type == 0
+        type = 7 + @buf.get_value(:U8, i)
+        i += 1
+      end
+
+      size = control & 0x1F
+      if size < 29 || type == 1
+        # noop
+      elsif size == 29
+        size = 29 + @buf.get_value(:U8, i)
+        i += 1
+      elsif size == 30
+        size = 285 + @buf.get_value(:U16, i)
+        i += 2
+      else # size == 31
+        # Safe overread by 1: because at least 65_821 bytes follow
+        size = 65_821 + (@buf.get_value(:U32, i) >> 8)
+        i += 3
+      end
+
+      @offset = i
+
+      case type
+      when 1 then decode_pointer(size)
+      when 2 then decode_string(size)
+      when 3 then decode_double
+      when 4 then decode_bytes(size)
+      when 5 then decode_uint16(size)
+      when 6 then decode_uint32(size)
+      when 7 then decode_map(size)
+      when 8 then decode_sint32(size)
+      when 9 then decode_uint64(size)
+      when 10 then decode_uint128(size)
+      when 11 then decode_array(size)
+      when 12 then decode_container
+      when 13 then decode_end
+      when 14 then decode_boolean(size)
+      when 15 then decode_float
+      else unknown_type(type)
+      end
+    end
+
+    private
+
+    def unknow_type(type)
+      raise EncodingError.new, "Unknown type #{type}"
+    end
+
+    def decode_pointer(size)
+      buf = @buf
+      i = @offset
+      ptr = size & 7
+      case size >> 3
+      when 0
+        ptr = (ptr << 8) | buf.get_value(:U8, i)
+        i += 1
+      when 1
+        ptr = ((ptr << 16) | buf.get_value(:U16, i)) + 2048
+        i += 2
+      when 2
+        # Unsafe overread by 1
+        ptr = ((ptr << 24) | (buf.get_value(:U32, i) >> 8)) + 526_336
+        i += 3
+      when 3
+        ptr = buf.get_value(:U32, i)
+        i += 4
+      end
+      ptr += @pointer_base
+
+      if @follow_pointers
+        @offset = ptr
+        x = decode
+      else
+        # FIXME
+        # x = Pointer.new ptr
+        x = ptr
+      end
+      @offset = i
+      x
+    end
+
+    def decode_boolean(size)
+      @offset += 1
+      size != 0
+    end
+
+    def decode_uint16(size)
+      return 0 if size == 0
+
+      # Unsafe overread by upto 1
+      x = @buf.get_value(:U16, @offset) >> (16 - (size << 3))
+      @offset += size
+      x
+    end
+
+    def decode_uint32(size)
+      return 0 if size == 0
+
+      # Unsafe overread by upto 3
+      x = @buf.get_value(:U32, @offset) >> (32 - (size << 3))
+      @offset += size
+      x
+    end
+
+    def decode_uint64(size)
+      return 0 if size == 0
+
+      # Unsafe overread by upto 7
+      x = @buf.get_value(:U64, @offset) >> (64 - (size << 3))
+      @offset += size
+      x
+    end
+
+    def decode_uint128(size)
+      if size <= 8
+        decode_uint64(size)
+      else
+        h = decode_uint64(size - 8)
+        l = decode_uint64(8)
+        (h << 64) | l
+      end
+    end
+
+    def decode_sint32(size)
+      return 0 if size == 0
+
+      x = if size == 4
+            @buf.get_value(:S32, @offset)
+          else
+            # Unsafe overread by upto 3
+            @buf.get_value(:U32, @offset) >> (32 - (size << 3))
+          end
+      @offset += size
+      x
+    end
+
+    def decode_float
+      x = @buf.get_value(:F32, @offset)
+      @offset += 4
+      x
+    end
+
+    def decode_double
+      x = @buf.get_value(:F64, @offset)
+      @offset += 8
+      x
+    end
+
+    def decode_string(size)
+      x = @buf.get_string(@offset, size, Encoding::UTF_8)
+      @offset += size
+      x
+    end
+
+    def decode_bytes(size)
+      x = @buf.get_string(@offset, size, Encoding::BINARY)
+      @offset += size
+      x
+    end
+
+    def decode_array(size)
+      x = []
+      i = 0
+      while i < size
+        x << decode
+        i += 1
+      end
+      x
+    end
+
+    def decode_map(size)
+      x = {}
+      i = 0
+      while i < size
+        k = decode
+        v = decode
+        x[k] = v
+        i += 1
+      end
+      x
+    end
+
+    def decode_end
+      raise 'TODO'
+    end
+
+    def decode_container
+      raise 'TODO'
+    end
+  end
+end
